1fc5dcb3949bdcf31f27b2420b05fb89f1dca90b
SimonRojas566 <romuloyerbarojas@gmail.com>
2026-08-24 08:09:40 -0500

    Cambio

--- PrototipoCodigo/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module pe.edu.upeu.prototipocodigo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens pe.edu.upeu.prototipocodigo to javafx.fxml;
-    exports pe.edu.upeu.prototipocodigo;
-}
